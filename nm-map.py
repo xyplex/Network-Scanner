7024482ccf9304d8aef9784b9cf80067384caecd
nmist-DT <n.mistry67@googlemail.com>
2020-04-27 11:51:26 +0100

    Adding Click @Decorators

--- nm-map.py
+++ nm-map.py
@@ -1,1 +1,17 @@
 import click
+from scapy.all import *
+
+@click.command()
+# @click.echo('TARGET SPECIFICATION:')
+@click.argument('text, nargs = -1')
+@click.option('--decrypt/--encrypt', '-d/-e')
+@click.option('--key', '-k', default=1)
+def caesar(text, decrypt, key):
+    text_string = ' '.join(text)
+    if decrypt:
+        key = -key
+    cyphertext = encrypt(text_string, key)
+    click.echo(cyphertext)
+
+if __name__ == '__main__':
+    caesar()
